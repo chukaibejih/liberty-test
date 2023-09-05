44fe0644268686ebffcddbc8c5425d446f2aa677
Ibejih <chukaibejih@gmail.com>
2023-09-05 22:06:40 +0100

    static

--- config/urls.py
+++ config/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 from rest_framework import permissions
 from drf_yasg.views import get_schema_view
@@ -39,4 +41,4 @@ urlpatterns = [
     path('blog/', include('blog.urls')),
 
     path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
